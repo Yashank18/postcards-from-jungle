b1731d8a3494403ca01b3ec2bbb2817251d8723d
yashank18 <yashank7696@gmail.com>
2024-07-19 13:54:22 +0530

    chore: basic cards and animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { MantineProvider } from "@mantine/core";
+import CardStack from "./CardStacks";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<MantineProvider>
+			<div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+				<CardStack />
+			</div>
+		</MantineProvider>
+	);
 }
 
-export default App
+export default App;
